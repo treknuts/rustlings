0736daf0a7c0863ee49f459a5404c7482f9a747e
treknuts <trevor.a.knutson@ndsu.edu>
2023-10-02 16:29:18 -0500

    Finished threads

--- exercises/threads/threads2.rs
+++ exercises/threads/threads2.rs
@@ -3,9 +3,7 @@
 // Building on the last exercise, we want all of the threads to complete their work but this time
 // the spawned threads need to be in charge of updating a shared value: JobStatus.jobs_completed
 
-// I AM NOT DONE
-
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -14,16 +12,14 @@ struct JobStatus {
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
     let mut handles = vec![];
     for _ in 0..10 {
         let status_shared = Arc::clone(&status);
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
-            // TODO: You must take an action before you update a shared value
-
-            let mut num = status_shared.lock().unwrap();
-            *num += 1;
+            let mut job_count = status_shared.lock().unwrap().jobs_completed;
+            job_count += 1;
         });
         handles.push(handle);
     }
@@ -31,6 +27,6 @@ fn main() {
         handle.join().unwrap();
         // TODO: Print the value of the JobStatus.jobs_completed. Did you notice anything
         // interesting in the output? Do you have to 'join' on all the handles?
-        println!("jobs completed {}", *status.lock().unwrap());
+        println!("jobs completed {}", status.lock().unwrap().jobs_completed);
     }
 }

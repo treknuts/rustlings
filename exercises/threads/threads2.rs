f8708d2668da7a05d6cdffee146408f33987a3a8
Trevor Knutson <trevor.a.knutson@ndsu.edu>
2023-10-02 14:47:00 -0500

    Finished iterators and left off on threads2

--- exercises/threads/threads2.rs
+++ exercises/threads/threads2.rs
@@ -21,7 +21,9 @@ fn main() {
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
             // TODO: You must take an action before you update a shared value
-            status_shared.jobs_completed += 1;
+
+            let mut num = status_shared.lock().unwrap();
+            *num += 1;
         });
         handles.push(handle);
     }
@@ -29,6 +31,6 @@ fn main() {
         handle.join().unwrap();
         // TODO: Print the value of the JobStatus.jobs_completed. Did you notice anything
         // interesting in the output? Do you have to 'join' on all the handles?
-        println!("jobs completed {}", ???);
+        println!("jobs completed {}", *status.lock().unwrap());
     }
 }
